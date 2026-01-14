6e5f267c08fc5d230fc6439cdfed8f2c223c5912
Barış Hamil <barishamil90@gmail.com>
2026-01-14 23:39:01 +0300

    Get prod to work

--- pkg/cache/db.go
+++ pkg/cache/db.go
@@ -3,6 +3,7 @@ package cache
 import (
 	"log"
 	"os"
+	"path/filepath"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -14,7 +15,16 @@ func InitDB() {
 	var err error
 	dbPath := os.Getenv("DATABASE_URL")
 	if dbPath == "" {
-		dbPath = "./db.sqlite"
+		configDir, err := os.UserConfigDir()
+		if err != nil {
+			log.Fatal("Failed to get user config dir:", err)
+		}
+		appDir := filepath.Join(configDir, "wails-dbman")
+		err = os.MkdirAll(appDir, 0755)
+		if err != nil {
+			log.Fatal("Failed to create app config dir:", err)
+		}
+		dbPath = filepath.Join(appDir, "db.sqlite")
 	}
 	DB, err = gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
 	if err != nil {
